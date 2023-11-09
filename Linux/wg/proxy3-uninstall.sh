5183c2b24b9f5c72e8e5f76079c55c47b778c3ff
Slaweekq <plakidin.vyacheslav@mail.ru>
2023-11-09 19:35:24 +0700

    1

--- Linux/wg/proxy3-uninstall.sh
+++
@@ -1,8 +0,0 @@
-# for deleting proxy3
-sudo systemctl stop 3proxy.service \
-  && sudo rm /etc/systemd/system/3proxy.service \
-  && sudo rm -rf /etc/3proxy \
-  && sudo rm /usr/bin/3proxy \
-  && sudo systemctl daemon-reload
-
-  # && sudo rm -rf /var/log/3proxy \
