542aa0d7ce8263528b4b85b95801a6c2ce48fb24
Slaweekq <plakidin.vyacheslav@mail.ru>
2023-12-06 14:37:13 +0700

    1

--- eth-node/ganache/start.sh
+++
@@ -1,3 +0,0 @@
-docker build -t ganache .
-docker run -p 8545:8545 -d --name ganache ganache
-docker ps
--- eth-node/start.sh
+++
@@ -1,6 +0,0 @@
-docker images -a
-docker service rm ethereum-node
-sudo systemctl daemon-reload
-
-docker compose down --rmi=all --volumes --remove-orphans
-docker rmi $(docker images -a -q)
