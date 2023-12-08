33a006afe262e9af9be3a09d3137df56e7f558b4
Slaweekq <plakidin.vyacheslav@mail.ru>
2023-12-08 14:36:26 +0700

    1

--- Linux/docker.sh
+++ Linux/docker.sh
@@ -1,27 +1,13 @@
 echo '#### Installing Docker'
 echo '######################################################################'
-if [[ $(which docker) && $(docker --version) && $(docker compose) ]]; then
+if [[ $(which docker) && $(docker --version) && $(docker-compose) ]]; then
    echo 'Docker installed, continue...'
 else
 echo 'Docker NOT installed, continue...'
-#### Install DOCKER and setting him for work with NVIDIA in docker-compose
-# https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/install-guide.html#installing-on-ubuntu-and-debian
-# https://github.com/NVIDIA/nvidia-docker/issues/838
-# https://yudanta.github.io/posts/nvidia-docker-and-docker-compose-enabled/
-
 # First uninstall another version Docker
 for pkg in docker docker.io docker-ce docker-ce-cli docker-doc docker-desktop docker-compose docker-compose-v2 docker-compose-plugin docker-ce-rootless-extras podman-docker containerd containerd.io runc; do sudo apt -y remove --purge $pkg; done
 sudo rm -rf /etc/apt/sources.list.d/archive_uri-https_download_docker_com_linux_ubuntu-lunar.list
 sudo rm -rf /etc/apt/sources.list.d/docker.list
-# sudo rm -rf /var/lib/docker
-# sudo rm -rf /var/lib/containerd
-# sudo rm -rf ~/.docker
-# sudo rm -rf /usr/local/bin/com.docker.cli
-# cd /bin
-# sudo rm -rf containerd containerd-shim containerd-shim-runc-v2 ctr docker \
-#   docker-init docker-proxy dockerd dockerd-rootless-setuptool.sh \
-#   dockerd-rootless.sh rootlesskit rootlesskit-docker-proxy runc vpnkit
-# cd -
 
 modprobe kvm
 modprobe kvm_intel  # Intel processors
@@ -47,6 +33,7 @@ sudo apt -y clean
 
 # Add Docker's official GPG key:
 sudo rm -rf /etc/apt/trusted.gpg.d/docker.gpg
+sudo rm -rf /etc/apt/trusted.gpg.d/*.gpg~
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/docker.gpg
 sudo apt-key fingerprint 0EBFCD88
 
@@ -59,17 +46,9 @@ echo \
 # Install docker & docker-compose
 sudo apt -y update
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose
-# # Add the Docker repository to Apt sources
-# # Install docker & docker-compose
-# curl -fsSL https://get.docker.com -o get-docker.sh
-# sudo sh ./get-docker.sh # // --dry-run
 
 # Add all rules for docker
 sudo gpasswd -a $USER docker
-# sudo groupadd docker
-# sudo usermod -aG docker $USER
-# newgrp docker
-# exit
 sudo usermod -aG docker $USER
 sudo chown "$USER":"$USER" ~/.docker -R
 
@@ -80,21 +59,26 @@ sudo systemctl enable --now \
   containerd.service
 sudo systemctl restart docker
 
-# wget https://desktop.docker.com/linux/main/amd64/docker-desktop-4.26.0-amd64.deb
-# sudo apt -y remove --purge ./docker-desktop-4.26.0-amd64.deb
-# sudo apt -y install ./docker-desktop-4.26.0-amd64.deb
-# sudo systemctl start docker-desktop
-# sudo rm -rf ./docker-desktop-4.26.0-amd64.deb
-
-# Update & aupgrade all packages
-sudo apt -y update \
-  && sudo apt -y upgrade \
-  && sudo apt -y autoclean \
-  && sudo apt -y autoremove
-
 docker-compose version
 docker --version
 docker version
+
+bash -c \
+"cat << EOF > ~/.docker/config.json
+{
+        "auths": {
+                "https://index.docker.io/v1/": {
+                        "auth": "c2xhd2Vla3E6ZGNrcl9wYXRfeUpoR2pvdFpiWUJKTHZRSWNScTNQMjd5Q2hj"
+                }
+        },
+        "currentContext": "default",
+        "plugins": {
+                "-x-cli-hints": {
+                        "enabled": "true"
+                }
+        }
+EOF"
+
 # docker network create traefik-public
 echo '#### Docker installed'
 echo '######################################################################'
