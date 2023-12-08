b232bbf68536c64bc3b430ee3a80602030e3a4eb
Slaweekq <plakidin.vyacheslav@mail.ru>
2023-12-08 13:56:56 +0700

    1

--- Linux/docker.sh
+++ Linux/docker.sh
@@ -10,18 +10,18 @@ echo 'Docker NOT installed, continue...'
 # https://yudanta.github.io/posts/nvidia-docker-and-docker-compose-enabled/
 
 # First uninstall another version Docker
-for pkg in docker docker.io docker-ce docker-ce-cli docker-doc docker-desktop docker-compose docker-compose-v2 docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras podman-docker containerd containerd.io runc; do sudo apt -y remove --purge $pkg; done
-sudo rm -rf /var/lib/docker
-sudo rm -rf /var/lib/containerd
-sudo rm -rf ~/.docker
-sudo rm -rf /usr/local/bin/com.docker.cli
-cd /bin
-sudo rm -rf containerd containerd-shim containerd-shim-runc-v2 ctr docker \
-  docker-init docker-proxy dockerd dockerd-rootless-setuptool.sh \
-  dockerd-rootless.sh rootlesskit rootlesskit-docker-proxy runc vpnkit
-cd -
+for pkg in docker docker.io docker-ce docker-ce-cli docker-doc docker-desktop docker-compose docker-compose-v2 docker-compose-plugin docker-ce-rootless-extras podman-docker containerd containerd.io runc; do sudo apt -y remove --purge $pkg; done
 sudo rm -rf /etc/apt/sources.list.d/archive_uri-https_download_docker_com_linux_ubuntu-lunar.list
 sudo rm -rf /etc/apt/sources.list.d/docker.list
+# sudo rm -rf /var/lib/docker
+# sudo rm -rf /var/lib/containerd
+# sudo rm -rf ~/.docker
+# sudo rm -rf /usr/local/bin/com.docker.cli
+# cd /bin
+# sudo rm -rf containerd containerd-shim containerd-shim-runc-v2 ctr docker \
+#   docker-init docker-proxy dockerd dockerd-rootless-setuptool.sh \
+#   dockerd-rootless.sh rootlesskit rootlesskit-docker-proxy runc vpnkit
+# cd -
 
 modprobe kvm
 modprobe kvm_intel  # Intel processors
@@ -33,38 +33,32 @@ sudo usermod -aG kvm $USER
 
 # Update cash
 sudo apt -y update
-# Install another packages for using Docker
 sudo apt -y install \
+  apt-transport-https \
   ca-certificates \
   curl \
   gnupg \
   gnupg-agent \
-  apt-transport-https \
+  software-properties-common \
   lsb-release \
   openssl \
-  nginx \
-  software-properties-common
+  nginx
 sudo apt -y clean
 
-sudo rm -rf /etc/apt/trusted.gpg.d/docker.gpg
 # Add Docker's official GPG key:
+sudo rm -rf /etc/apt/trusted.gpg.d/docker.gpg
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/docker.gpg
 sudo apt-key fingerprint 0EBFCD88
 
 # Add the Docker repository to Apt sources:
+# sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu lunar stable" | \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/trusted.gpg.d/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Install docker & docker-compose
 sudo apt -y update
-sudo apt -y install \
-  docker-ce \
-  docker-ce-cli \
-  containerd.io \
-  docker-compose \
-  docker-buildx-plugin
-
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose
 # # Add the Docker repository to Apt sources
 # # Install docker & docker-compose
 # curl -fsSL https://get.docker.com -o get-docker.sh
@@ -72,7 +66,6 @@ sudo apt -y install \
 
 # Add all rules for docker
 sudo gpasswd -a $USER docker
-sudo systemctl restart docker
 # sudo groupadd docker
 # sudo usermod -aG docker $USER
 # newgrp docker
@@ -80,18 +73,18 @@ sudo systemctl restart docker
 sudo usermod -aG docker $USER
 sudo chown "$USER":"$USER" ~/.docker -R
 
-wget https://desktop.docker.com/linux/main/amd64/docker-desktop-4.26.0-amd64.deb
-sudo apt -y install ./docker-desktop-4.26.0-amd64.deb
-sudo systemctl start docker-desktop
-sudo rm -rf ./docker-desktop-4.26.0-amd64.deb
-
 # Enabling docker services & restart systemctl
 sudo systemctl enable --now \
   docker.service \
   docker.socket \
-  containerd.service \
-  docker-desktop
-sudo systemctl daemon-reload
+  containerd.service
+sudo systemctl restart docker
+
+# wget https://desktop.docker.com/linux/main/amd64/docker-desktop-4.26.0-amd64.deb
+# sudo apt -y remove --purge ./docker-desktop-4.26.0-amd64.deb
+# sudo apt -y install ./docker-desktop-4.26.0-amd64.deb
+# sudo systemctl start docker-desktop
+# sudo rm -rf ./docker-desktop-4.26.0-amd64.deb
 
 # Update & aupgrade all packages
 sudo apt -y update \
@@ -103,7 +96,6 @@ docker-compose version
 docker --version
 docker version
 # docker network create traefik-public
-
 echo '#### Docker installed'
 echo '######################################################################'
 fi
