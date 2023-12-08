5fc172ded5350678924d32f2aa78211dcb51a684
Slaweekq <plakidin.vyacheslav@mail.ru>
2023-12-08 16:04:04 +0700

    «new»

--- Linux/docker.sh
+++ Linux/docker.sh
@@ -5,9 +5,18 @@ if [[ $(which docker) && $(docker --version) && $(docker-compose) ]]; then
 else
 echo 'Docker NOT installed, continue...'
 # First uninstall another version Docker
-for pkg in docker docker.io docker-ce docker-ce-cli docker-doc docker-compose docker-compose-v2 docker-ce-rootless-extras podman-docker containerd runc; do sudo apt -y remove --purge $pkg; done
+for pkg in docker docker.io docker-ce docker-ce-cli docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt -y remove --purge $pkg; done
 sudo rm -rf /etc/apt/sources.list.d/archive_uri-https_download_docker_com_linux_ubuntu-lunar.list
 sudo rm -rf /etc/apt/sources.list.d/docker.list
+sudo rm -rf /var/lib/docker
+sudo rm -rf /var/lib/containerd
+sudo rm -rf ~/.docker
+sudo rm -rf /usr/local/bin/com.docker.cli
+cd /bin
+sudo rm -rf containerd containerd-shim containerd-shim-runc-v2 ctr docker \
+  docker-init docker-proxy dockerd dockerd-rootless-setuptool.sh \
+  dockerd-rootless.sh rootlesskit rootlesskit-docker-proxy runc vpnkit
+cd -
 
 modprobe kvm
 modprobe kvm_intel  # Intel processors
@@ -17,19 +26,37 @@ lsmod | grep kvm
 ls -al /dev/kvm
 sudo usermod -aG kvm $USER
 
-# Update cash
-sudo apt -y update
+sudo apt -y update \
+  && sudo apt -y upgrade \
+  && sudo apt -y autoremove \
+  && sudo apt -y autoclean
+
 sudo apt -y install \
+  apt-transport-https \
+  ca-certificates \
+  curl \
+  gnupg \
+  gnupg-agent \
+  software-properties-common \
   lsb-release \
   openssl \
   nginx
 sudo apt -y clean
 
-# # Add the Docker repository to Apt sources
-# # Install docker & docker-compose
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh ./get-docker.sh # // --dry-run
-rm -rf ./get-docker.sh
+# Add Docker's official GPG key:
+sudo rm -rf /etc/apt/trusted.gpg.d/docker.gpg
+sudo rm -rf /etc/apt/trusted.gpg.d/*.gpg~
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/docker.gpg
+sudo apt-key fingerprint 0EBFCD88
+
+# Add the Docker repository to Apt sources:
+sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+# Install docker & docker-compose
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose
+# curl -fsSL https://get.docker.com -o get-docker.sh
+# sudo sh ./get-docker.sh # // --dry-run
+# rm -rf ./get-docker.sh
 
 # Add all rules for docker
 sudo gpasswd -a $USER docker
@@ -37,7 +64,7 @@ sudo usermod -aG docker $USER
 sudo chown "$USER":"$USER" ~/.docker -R
 
 # Enabling docker services & restart systemctl
-sudo systemctl enable --now docker docker.socket containerd
+sudo systemctl enable --now docker.service docker.socket containerd.service
 sudo systemctl restart docker
 # echo 'alias docker-compose="docker compose"' >> ~/.bashrc
 # docker network create traefik-public
